5401eea88a46ae0eec33c271f61415325c3f4564
Oliver Veits <oliver.veits@gmail.com>
2018-08-01 01:52:19 +0200

    unsubscribes for getting rid of a memory leak

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AppService } from './app.service';
 import { SafePost } from './safe-post.interface';
-import { interval} from 'rxjs';
+import { interval, Subscription} from 'rxjs';
 
 
 @Component({
@@ -9,17 +9,29 @@ import { interval} from 'rxjs';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   posts: SafePost[];
+  subInner: Subscription;
+  subOuter: Subscription;
+
 
   constructor(private appService: AppService) {}
 
   ngOnInit() {
-    interval(10000).subscribe((counter) => {
-      this.appService.getAll().subscribe(posts => {
+    this.subOuter = interval(10000).subscribe((counter) => {
+      if (this.subInner !== undefined) {
+        console.log('unsubscribing subInner');
+        this.subInner.unsubscribe();
+      }
+      this.subInner = this.appService.getAll().subscribe(posts => {
         this.posts = posts;
       });
       console.log(counter + ': read restItems');
     });
+  }
+
+  ngOnDestroy() {
+    this.subInner.unsubscribe();
+    this.subOuter.unsubscribe();
   }
 }
