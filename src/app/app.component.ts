279c87526f77f0f7dcfac0114b764d7ac78718b1
Oliver Veits <oliver.veits@gmail.com>
2018-08-04 16:03:18 +0200

    exponential backoff with a combination of async pipe and subscriptions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable, pipe, Subscription } from 'rxjs';
 import { AppService } from './app.service';
 import { SafePost } from './safe-post.interface';
+
+// exponentialBackoff:
+import { fromEvent } from 'rxjs';
+import { sampleTime, startWith, switchMapTo, tap, map } from 'rxjs/operators';
+import { intervalBackoff } from 'backoff-rxjs';
+
+export const INITIAL_INTERVAL_MS = 5000; // 5 sec, choose larger than mean response time of REST service called
+export const MAX_INTERVAL_MS = 60000; // 1 min
 
 
 @Component({
@@ -9,18 +17,68 @@ import { SafePost } from './safe-post.interface';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
+  posts$$: Observable<Observable<SafePost[]>>;
   posts$: Observable<SafePost[]>;
+  posts: SafePost[];
+  mySubscription: Subscription;
+  exponentialBackoffTimer: Observable<number>;
 
   constructor(private appService: AppService) {}
 
   ngOnInit() {
-    this.posts$ = this.getRestItems$();
+    // this.posts$ = this.getRestItems$();
+    this.exponentialBackoffTimer =
+      fromEvent(document, 'mousemove').pipe(
+
+        // There could be many mousemoves, we'd want to sample only
+        // with certain frequency
+        sampleTime(INITIAL_INTERVAL_MS),
+
+        // Start immediately
+        startWith(null),
+
+        // Resetting exponential interval operator
+        switchMapTo(intervalBackoff({
+          backoffDelay: (iteration, initialInterval) => Math.pow(1.2, iteration) * initialInterval,
+          initialInterval: INITIAL_INTERVAL_MS,
+          maxInterval: MAX_INTERVAL_MS
+        })),
+
+        // attaching the function that is to be reset:
+        tap( n => {
+          console.log('iteration since reset: ' + n);
+          this.mySubscription = this.reSubscribeToGetAndAssignPosts$(
+            this.mySubscription,
+            this.getAndAssignPosts$()
+          );
+        }),
+      );
   }
 
-  // Read all REST Items
-  getRestItems$(): Observable<SafePost[]> {
-    return this.appService.getAll()
-      .pipe(posts => posts);
+  /*
+    removes old subscription and returns new subscription
+    Parameters: mySybsription
+    Returns: Subscription
+  */
+  reSubscribeToGetAndAssignPosts$(mySubscription: Subscription, myObservable$: Observable<void>): Subscription {
+    // remove old subscription:
+    if (mySubscription !== undefined) {
+      console.log('unsubscribing mySubscription');
+      mySubscription.unsubscribe();
+    }
+
+    // assign new subscription:
+    return myObservable$.subscribe();
+  }
+
+  getAndAssignPosts$(): Observable<void> {
+    return this.appService.getAll().pipe(map(posts => {
+      this.posts = posts;
+    }));
+  }
+
+  ngOnDestroy() {
+    this.mySubscription.unsubscribe();
   }
 }
