209c7d4e539885050d178b357de0567d84178162
Oliver Veits <oliver.veits@gmail.com>
2018-09-05 01:55:31 +0200

    adding flatMap and adapting types from nested Observables to simple Observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,10 @@ import { SafePost } from './safe-post.interface';
 
 // intervalBackoff:
 import { fromEvent } from 'rxjs';
-import { sampleTime, startWith, switchMapTo, map, switchMap } from 'rxjs/operators';
+import { sampleTime, startWith, switchMapTo, map, flatMap } from 'rxjs/operators';
 import { intervalBackoff } from 'backoff-rxjs';
 
-export const INITIAL_INTERVAL_MS = 5000; // 5 sec, choose larger than mean response time of REST service called
+export const INITIAL_INTERVAL_MS = 1000; // 1 sec, choose larger than mean response time of REST service called
 export const MAX_INTERVAL_MS = 60000; // 1 min
 
 
@@ -42,7 +42,7 @@ export class AppComponent implements OnInit {
           maxInterval: MAX_INTERVAL_MS
         })),
 
-        switchMap( n => {
+        flatMap( n => {
           console.log('iteration since reset: ' + n);
           this.n = n;
           return this.getAndAssignPosts$();
